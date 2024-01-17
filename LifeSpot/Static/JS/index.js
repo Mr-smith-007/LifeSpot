fe92ffbd5f96dadfdfb11f70b65777e3961e8b8d
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-01-17 16:46:43 +0300

    29.1.1

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -18,3 +18,19 @@ else
 for (let result of session) {
     console.log(result)
 }
+
+function filterContent() {
+
+    let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
+
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++) {
+        let videoText = elements[i].querySelector(".video-title").innerText;
+        if (!videoText.toLowerCase().includes(inputString.toLowerCase())) {
+            elements[i].style.display = 'none';
+        } else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
