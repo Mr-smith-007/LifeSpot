23821894c5ef113e63bd1b51253e5761d5826822
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-01-17 16:10:25 +0300

    28.6.4

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,9 +1,20 @@
-﻿let age = prompt("Введите ваш возраст");
+﻿let session = new Map();
+session.set("userAgent", window.navigator.userAgent);
+session.set("age", prompt("Пожалуйста, введите ваш возраст?"));
 
-if (age >= 18) {    
-    alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());
+if (session.get("age") >= 18)
+{
+    let startDate = new Date().toLocaleString();
+    alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+    session.set("startDate", startDate)
 }
-else {    
+else
+{
     alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
     window.location.href = "http://www.google.com"
+    a = true + 20 + "name"
+}
+
+for (let result of session) {
+    console.log(result)
 }
