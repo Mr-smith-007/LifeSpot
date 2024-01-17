af5a22f86bd1d925d628683dc9fd216c3a108655
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-01-17 16:57:13 +0300

    29.1.2

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,22 +1,22 @@
-﻿let session = new Map();
-session.set("userAgent", window.navigator.userAgent);
-session.set("age", prompt("Пожалуйста, введите ваш возраст?"));
+﻿function handleSession() {
+    let session = new Map();
+    session.set("userAgent", window.navigator.userAgent);
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"));
 
-if (session.get("age") >= 18)
-{
-    let startDate = new Date().toLocaleString();
-    alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
-    session.set("startDate", startDate)
-}
-else
-{
-    alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-    window.location.href = "http://www.google.com"
-    a = true + 20 + "name"
-}
+    if (session.get("age") >= 18) {
+        let startDate = new Date().toLocaleString();
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+        session.set("startDate", startDate)
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+        a = true + 20 + "name"
+    }
 
-for (let result of session) {
-    console.log(result)
+    for (let result of session) {
+        console.log(result)
+    }
 }
 
 function filterContent() {
