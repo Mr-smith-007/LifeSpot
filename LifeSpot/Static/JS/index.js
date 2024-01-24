d6b41b2cb549cca1e3079b181aacf04daa201b95
Алексей Репников <mr_smith_007@mail.ru>
2024-01-24 11:41:47 +0300

    29.5.5

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,27 +1,33 @@
-﻿let sessionLog = function logSession(session) {
+﻿let session = new Map();
+
+
+let sessionLog = function logSession() {
     for (let result of session) {
         console.log(result)
     }
 }
 
 
-function handleSession() {
-    let session = new Map();
-    session.set("userAgent", window.navigator.userAgent);
-    session.set("age", prompt("Пожалуйста, введите ваш возраст?"));
+function checkAge() {
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
 
     if (session.get("age") >= 18) {
-        let startDate = new Date().toLocaleString();
-        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
-        session.set("startDate", startDate)
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
     }
     else {
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
         window.location.href = "http://www.google.com"
     }
-
-    return session;
 }
+
+
+function handleSession() {
+    
+    session.set("userAgent", window.navigator.userAgent);
+    session.set("startDate", new Date().toLocaleString());
+          
+}
+
 
 var filterContent = function () {
 
