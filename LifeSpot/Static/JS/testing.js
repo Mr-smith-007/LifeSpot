889d5a0a02c35522f8118210133051a1e7356150
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-01-30 16:06:12 +0300

    30.3.2

--- LifeSpot/Static/JS/testing.js
+++ LifeSpot/Static/JS/testing.js
@@ -1,3 +1,10 @@
-﻿let elements = document.getElementsByTagName('*');
+﻿const saveInput = function () {    
+    let currentInput = document.getElementsByTagName('input')[0].value.toLowerCase();
+        
+    alert('Последний ввод: ' + this.lastInput + '\n' + 'Текущий ввод: ' + currentInput);
+         
+    this.lastInput = currentInput;
+}
 
-alert(`Количество элементов на странице:  ${elements.length}`);
+//let elements = document.getElementsByTagName('*');
+//alert(`Количество элементов на странице:  ${elements.length}`);
