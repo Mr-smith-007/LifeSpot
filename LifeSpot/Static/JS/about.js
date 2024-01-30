920094e7986676cb7effbc42f9a0e8abd79801d8
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-01-30 15:01:25 +0300

    30.2.4

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -1,25 +1,39 @@
-function getReview() {
-	let review = {}
-		
-	review["userName"] = prompt("Как вас зовут ?")
-	if (review["userName"] == null) {
-		return
-	}
-		
-	review["comment"] = prompt("Напишите свой отзыв")
-	if (review["comment"] == null) {
-		return
-	}
-		
-	review["date"] = new Date().toLocaleString()
-		
-	writeReview(review)
+function getComment() {    
+    let comment = {}
+        
+    comment.author = prompt("Как вас зовут ?")
+    if (comment.author == null) {
+        return
+    }
+        
+    comment.text = prompt("Оставьте отзыв")
+    if (comment.text == null) {
+        return
+    }
+       
+    comment.date = new Date().toLocaleString()
+        
+    let enableLikes = confirm('Разрешить пользователям оценивать ваш отзыв?')
+
+    if (enableLikes) {        
+        let review = Object.create(comment)        
+        review.rate = 0;                
+        writeReview(review)
+    } else {        
+        writeReview(comment)
+    }
 }
 
 
 const writeReview = review => {
+	let likeCounter = '';
+		
+	if (review.hasOwnProperty('rate')) {
+		likeCounter += '           <b style="color: chocolate">Рейтинг:</b>   ' + review.rate;
+	}		
+
 	document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
-		`<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
-		`<p>${review['comment']}</p>` +
+		`<p> <i> <b>${review['author']}</b>  ${review['date']}${likeCounter}</i></p>` +
+		`<p>${review['text']}</p>` +
 		'</div>';
 }
