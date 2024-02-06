a8bedd78f2a316f2938b3bfbcb8ae9ea3c247b74
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-02-06 19:08:34 +0300

    Не работает листание мышкой

--- LifeSpot/Startup.cs
+++ LifeSpot/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace LifeSpot
@@ -26,6 +27,11 @@ namespace LifeSpot
                 endpoints.MapCss();
                 endpoints.MapJs();
                 endpoints.MapHtml();
+                app.UseStaticFiles(new StaticFileOptions
+                {
+                    FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "Static", "Images")),
+                    RequestPath = "/Static/Images"
+                });
             });
         }
     }
--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -1,64 +1,155 @@
-﻿function Comment() {	
-	this.author = prompt("Как вас зовут ?")
-	if (this.author == null) {
-		this.empty = true
-		return
-	}
-	
-	this.text = prompt("Оставьте отзыв")
-	if (this.text == null) {
-		this.empty = true
-		return
-	}	
+﻿function Comment() {
+    this.author = prompt("Как вас зовут ?")
+    if (this.author == null) {
+        this.empty = true
+        return
+    }
 
-	this.date = new Date().toLocaleString()
+    this.text = prompt("Оставьте отзыв")
+    if (this.text == null) {
+        this.empty = true
+        return
+    }
+
+    this.date = new Date().toLocaleString()
 }
 
 
 function addComment() {
-	let comment = new Comment()
-		
-	if (comment.empty) {
-		return;
-	}	
+    let comment = new Comment()
 
-	let enableLikes = confirm('Разрешить пользователям оценивать ваш отзыв?')
+    if (comment.empty) {
+        return;
+    }
 
-	if (enableLikes) {		
-		let review = Object.create(comment)		
-		review.rate = 0;		
-		writeReview(review)
-	} else {		
-		writeReview(comment)
-	}
+    let enableLikes = confirm('Разрешить пользователям оценивать ваш отзыв?')
+
+    if (enableLikes) {
+        let review = Object.create(comment)
+        review.rate = 0;
+        writeReview(review)
+    } else {
+        writeReview(comment)
+    }
 }
 
 
 const writeReview = review => {
-	let likeCounter = '';
-		
-	if (review.hasOwnProperty('rate')) {
-		let commentId = Math.random();
-		likeCounter += '<button id="' + commentId + '" style="border: none" onclick="addLike(this.id)">' + `❤️ ${review.rate}</button>`
-	}		
+    let likeCounter = '';
 
-	document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
-		`<p> <i> <b>${review['author']}</b>  ${review['date']}${likeCounter}</i></p>` +
-		`<p>${review['text']}</p>` +
-		'</div>';
+    if (review.hasOwnProperty('rate')) {
+        let commentId = Math.random();
+        likeCounter += '<button id="' + commentId + '" style="border: none" onclick="addLike(this.id)">' + `❤️ ${review.rate}</button>`
+    }
+
+    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+        `<p> <i> <b>${review['author']}</b>  ${review['date']}${likeCounter}</i></p>` +
+        `<p>${review['text']}</p>` +
+        '</div>';
 }
 
 
-function addLike(id) {	
-	let element = document.getElementById(id);
-		
-	let array = element.innerText.split(' ')
-	
-	let resultNum = parseInt(array[array.length - 1], 10);
-		
-	resultNum += 1
-		
-	array[array.length - 1] = `${resultNum}`
-		
-	element.innerText = array.join(' ')
+function addLike(id) {
+    let element = document.getElementById(id);
+
+    let array = element.innerText.split(' ')
+
+    let resultNum = parseInt(array[array.length - 1], 10);
+
+    resultNum += 1
+
+    array[array.length - 1] = `${resultNum}`
+
+    element.innerText = array.join(' ')
+}
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+let initMouseX;
+var currSlide = 1;
+let isMonitored;
+let distance = 0;
+let slides = document.getElementsByClassName("mySlides");
+
+function startListen() {
+    console.log('startListen');
+    showSlides(currSlide);
+    ssContainer = document.getElementById('slideContainer');
+
+    ssContainer.addEventListener('mousedown', (event) => {
+        event.preventDefault();
+        console.log('mouse is down');
+        isMonitored = true;
+        initMouseX = event.clientX;
+        ssContainer.addEventListener('mousemove', (e) => {
+            let currentMouseX = e.clientX;
+            if (isMonitored) {
+                distance = currentMouseX - initMouseX;
+                let trans = 'translateX(' + distance + 'px)';
+                for (i = 0; i < slides.length; i++) {
+                    if (slides[i].style.display == "block") {
+                        currSlide = i + 1;
+                    }
+                }
+
+                slides[currSlide - 1].style.transform = trans;     
+                if (currentMouseX - initMouseX < -200) {
+                    initMouseX = currentMouseX;
+                    plusSlides(1);
+                    currSlide += 1;
+                    if (currSlide > 4) { currSlide = 1 }
+                }
+                if (currentMouseX - initMouseX > 200) {
+                    initMouseX = currentMouseX;
+                    plusSlides(-1);
+                    currSlide -= 1;
+                    if (currSlide < 1) { currSlide = 4 }
+                }
+            }
+        });
+        ssContainer.addEventListener('mouseup', (e) => {
+            isMonitored = false;
+            slides[currSlide - 1].style.transform = "";
+        });
+
+        ssContainer.addEventListener('mouseleave', (e) => {
+            isMonitored = false;
+            slides[currSlide - 1].style.transform = "";
+        });
+
+
+    });
+
+    ssContainer.addEventListener('mouseup', (evt) => { evt.preventDefault(); console.log('mouse is up'); });
 }
